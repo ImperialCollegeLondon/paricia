e846a35e7d84e0e8c0eb835589afe0bd1023b9df
paul19pv <paul19pv@gmail.com>
2018-07-25 15:52:28 -0500

    Reinicio de las migraciones

--- sedc/settings.py
+++ sedc/settings.py
@@ -97,7 +97,7 @@ WSGI_APPLICATION = 'sedc.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'db_sedc',
+        'NAME': 'db_sedc_alt',
         'USER': 'postgres',
         'PASSWORD': 'dev123*',
         'HOST': '192.168.19.8',
