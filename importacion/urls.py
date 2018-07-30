3a06e9f8b79c90678b748699c0b3dc41e586abfd
paul19pv <paul19pv@gmail.com>
2018-07-30 08:49:49 -0500

    correción en la consulta del registro de la lectura automática

--- importacion/urls.py
+++ importacion/urls.py
@@ -14,5 +14,6 @@ urlpatterns = [
     re_path(r'importacion/(?P<pk>[0-9]+)/delete/$', views.ImportacionDelete.as_view(), name='importacion_delete'),
     re_path(r'importacion/lectura/$', views.lectura_automatica, name='lectura'),
     path('importacion/automatica', views.ListAutomatico.as_view(), name='importacion_automatica'),
+    path('importacion/automatica/<int:page>', views.ListAutomatico.as_view(), name='importacion_automatica'),
     re_path(r'^ajax/formatos', views.lista_formatos, name='formatos')
 ]
