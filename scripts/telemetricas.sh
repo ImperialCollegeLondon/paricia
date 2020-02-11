7c22efb7a325cacb1e379fd7af2b04ea5bceb9c9
Pablo Jacome <pxjacomex@gmail.com>
2020-02-11 14:32:01 -0500

    Servicio para lectura telemetricas

---
+++ scripts/telemetricas.sh
@@ -0,0 +1,3 @@
+/home/developer/web/ENV/sedc_web_env/bin/python3.6 /home/developer/web/sedc_web/manage.py runscript loggernet
+/home/developer/web/ENV/sedc_web_env/bin/python3.6 /home/developer/web/sedc_web/manage.py runscript vaisala
+
