76391aa615b7b15b8df007658e153d88cc56710d
paul19pv <paul19pv@gmail.com>
2020-02-14 17:33:45 -0500

    Reporte Anuario Precipitación

--- reportes/titulos.py
+++ reportes/titulos.py
@@ -1,14 +1,130 @@
 from variable.models import Variable, Unidad
-from estacion.models import Estacion
+# librerias para manejar los archivos EXCEL
+from openpyxl.styles import Font, Alignment, Border, Side, PatternFill
 
 
-class Titulos():
-    def titulo_grafico(self, variable):
+class Titulos:
+    # Estilo de Fuentes
+    fonts = {
+        'font_bold_11': Font(bold=True),
+        'font_bold_10': Font(bold=True, size=10),
+        'font_10': Font(size=10),
+        'font_11': Font(size=11),
+        'font_8': Font(size=8)
+    }
+    # Alineacion de las celdas
+    alignments = {
+        'center': Alignment(horizontal="center", vertical="center"),
+        'right': Alignment(horizontal="right", vertical="center"),
+        'left': Alignment(horizontal="left", vertical="center"),
+        'wrap': Alignment(horizontal="center", vertical="center", wrap_text=True)
+    }
+    thin = Side(border_style="thin", color="000000")
+    medium = Side(border_style="medium", color="000000")
+    # bordes
+    borders = {
+        'border_thin': Border(top=thin, left=thin, bottom=thin, right=thin),
+        'border_medium': Border(top=medium, left=medium, right=medium, bottom=medium)
+    }
+    # relleno
+    colors = {
+        'orange': PatternFill("solid", fgColor="FDE9D9"),
+        'light_salmon': PatternFill("solid", fgColor="FFA07A")
+    }
+
+    @staticmethod
+    def titulo_grafico(variable):
         # returns var_nombre given var_id
         consulta = list(Variable.objects.filter(var_id=variable))
 
         return consulta[0]
 
-    def titulo_unidad(self, variable):
-        var=Variable.objects.get(var_id=variable)
+    @staticmethod
+    def titulo_unidad(variable):
+        var = Variable.objects.get(var_id=variable)
         return var.uni_id.uni_sigla
+
+    def set_encabezado_excel(self, ws, estacion, periodo):
+        fila = 1
+        col = 1
+        col_fin = 11
+
+        ws.merge_cells(start_row=fila, start_column=col, end_row=fila, end_column=col+2)
+        title = ws.cell(row=fila, column=col)
+        title.value = estacion.est_codigo
+        self.set_style(cell=title, font='font_bold_11', alignment='center',
+                       border='border_thin', fill='light_salmon')
+
+        ws.merge_cells(start_row=fila, start_column=col+3, end_row=fila, end_column=col+7)
+        title = ws.cell(row=fila, column=col+3)
+        title.value = estacion.est_nombre
+        self.set_style(cell=title, font='font_bold_11', alignment='center',
+                       border='border_thin', fill='light_salmon')
+
+        ws.merge_cells(start_row=fila, start_column=col+8, end_row=fila, end_column=col_fin)
+        title = ws.cell(row=fila, column=col+8)
+        title.value = periodo
+        self.set_style(cell=title, font='font_bold_11', alignment='center',
+                       border='border_thin', fill='light_salmon')
+
+        fila += 1
+
+        ws.merge_cells(start_row=fila, start_column=col, end_row=fila, end_column=col_fin)
+        title = ws.cell(row=fila, column=col)
+        title.value = 'Coordenadas Geográfica UTM (DATUM WGS 84)'
+        self.set_style(cell=title, font='font_bold_11', alignment='center',
+                       border='border_thin')
+
+        fila += 1
+
+        cell = ws.cell(row=fila, column=col)
+        cell.value = "Latitud"
+        self.set_style(cell=cell, font='font_bold_10', alignment='center',
+                       border='border_thin')
+
+        ws.merge_cells(start_row=fila, start_column=col + 1, end_row=fila, end_column=col + 3)
+        title = ws.cell(row=fila, column=col + 1)
+        title.value = estacion.est_latitud
+        self.set_style(cell=title, font='font_10', alignment='center',
+                       border='border_thin')
+
+        cell = ws.cell(row=fila, column=col+4)
+        cell.value = "Longitud"
+        self.set_style(cell=cell, font='font_bold_10', alignment='center',
+                       border='border_thin')
+
+        ws.merge_cells(start_row=fila, start_column=col + 5, end_row=fila, end_column=col + 7)
+        title = ws.cell(row=fila, column=col + 5)
+        title.value = estacion.est_longitud
+        self.set_style(cell=title, font='font_10', alignment='center',
+                       border='border_thin')
+
+        cell = ws.cell(row=fila, column=col + 8)
+        cell.value = "Altura"
+        self.set_style(cell=cell, font='font_bold_10', alignment='center',
+                       border='border_thin')
+
+        ws.merge_cells(start_row=fila, start_column=col + 9, end_row=fila, end_column=col_fin)
+        title = ws.cell(row=fila, column=col + 9)
+        title.value = estacion.est_altura
+        self.set_style(cell=title, font='font_10', alignment='center',
+                       border='border_thin')
+
+    @staticmethod
+    def get_mes_anio(int_mes):
+        # meses = ['Enero', 'Febrero','Marzo', 'Abril', 'Mayo', 'Junio', 'Julio','Agosto',
+        # 'Septiembre','Octubre','Noviembre','Diciembre']
+        meses = ['ENE', 'FEB', 'MAR', 'ABR', 'MAY', 'JUN', 'JUL', 'AGO', 'SEP', 'OCT',
+                 'NOV', 'DIC']
+
+        return meses[int_mes-1]
+
+    def set_style(self, cell, font=None, alignment=None, border=None, fill = None):
+        if font is not None:
+            cell.font = self.fonts[font]
+        if alignment is not None:
+            cell.alignment = self.alignments[alignment]
+        if border is not None:
+            cell.border = self.borders[border]
+        if fill is not None:
+            cell.fill = self.colors[fill]
--- reportes/typeII.py
+++ reportes/typeII.py
@@ -8,6 +8,7 @@ from django.db.models import Avg, Max, Min
 from django.db.models import FloatField
 import calendar
 
+from openpyxl.chart import BarChart, Series, Reference
 
 # clase para anuario de la variable PRE
 class TypeII(Titulos):
@@ -80,3 +81,148 @@ class TypeII(Titulos):
             div = opy.plot(figure, auto_open=False, output_type='div')
             return div
         return False
+
+    def tabla_excel(self, ws, estacion, periodo):
+        fila = 5
+        col_fin = 11
+
+        ws.merge_cells(start_row=fila, start_column=1, end_row=fila, end_column=col_fin)
+        subtitle = ws.cell(row=fila, column=1)
+        subtitle.value = "PRECIPITACIÓN - VALORES MENSUALES Y MÁXIMOS DIARIOS"
+        self.set_style(cell=subtitle, font='font_bold_10', alignment='center',
+                       border='border_thin', fill='light_salmon')
+        fila += 1
+
+        ws.merge_cells(start_row=fila, start_column=1, end_row=fila+2, end_column=1)
+        cell = ws.cell(row=fila, column=1)
+        cell.value = "MES"
+        self.set_style(cell=cell, font='font_10', alignment='center',
+                       border='border_thin')
+
+        ws.merge_cells(start_row=fila, start_column=8, end_row=fila + 2, end_column=8)
+        cell = ws.cell(row=fila, column=8)
+        cell.value = "Cantidad de días con precipitación"
+        self.set_style(cell=cell, font='font_8', alignment='wrap',
+                       border='border_thin')
+
+        ws.merge_cells(start_row=fila, start_column=2, end_row=fila, end_column=7)
+        cell = ws.cell(row=fila, column=2)
+        cell.value = "Precipitación (mm)"
+        self.set_style(cell=cell, font='font_10', alignment='center',
+                       border='border_thin')
+        fila += 1
+        ws.merge_cells(start_row=fila, start_column=2, end_row=fila+1, end_column=2)
+        cell = ws.cell(row=fila, column=2)
+        cell.value = "Mensual"
+        self.set_style(cell=cell, font='font_10', alignment='center',
+                       border='border_thin')
+
+        ws.merge_cells(start_row=fila, start_column=3, end_row=fila + 1, end_column=3)
+        cell = ws.cell(row=fila, column=3)
+        cell.value = "Medía Histórica"
+        self.set_style(cell=cell, font='font_10', alignment='wrap',
+                       border='border_thin')
+
+        ws.merge_cells(start_row=fila, start_column=4, end_row=fila + 1, end_column=4)
+        cell = ws.cell(row=fila, column=4)
+        cell.value = "Máximo Histórica"
+        self.set_style(cell=cell, font='font_10', alignment='wrap',
+                       border='border_thin')
+
+        ws.merge_cells(start_row=fila, start_column=5, end_row=fila + 1, end_column=5)
+        cell = ws.cell(row=fila, column=5)
+        cell.value = "Mínimo Histórica"
+        self.set_style(cell=cell, font='font_10', alignment='wrap',
+                       border='border_thin')
+
+        ws.merge_cells(start_row=fila, start_column=6, end_row=fila, end_column=7)
+        cell = ws.cell(row=fila, column=6)
+        cell.value = "Máxima en"
+        self.set_style(cell=cell, font='font_10', alignment='center',
+                       border='border_thin')
+
+        fila += 1
+
+        cell = ws.cell(row=fila, column=6)
+        cell.value = "24H"
+        self.set_style(cell=cell, font='font_10', alignment='center',
+                       border='border_thin')
+
+        cell = ws.cell(row=fila, column=7)
+        cell.value = "Día"
+        self.set_style(cell=cell, font='font_10', alignment='center',
+                       border='border_thin')
+
+        matriz = self.matriz(estacion, periodo)
+        fila += 1
+
+        for item in matriz:
+            cell = ws.cell(row=fila, column=1)
+            cell.value = self.get_mes_anio(item.pre_mes)
+            self.set_style(cell=cell, font='font_10', alignment='left',
+                           border='border_thin')
+            cell = ws.cell(row=fila, column=2)
+            cell.value = item.pre_suma
+            self.set_style(cell=cell, font='font_10', alignment='left',
+                           border='border_thin')
+
+            cell = ws.cell(row=fila, column=6)
+            cell.value = item.pre_maximo
+            self.set_style(cell=cell, font='font_10', alignment='left',
+                           border='border_thin')
+
+            cell = ws.cell(row=fila, column=7)
+            cell.value = item.pre_maximo_dia
+            self.set_style(cell=cell, font='font_10', alignment='left',
+                           border='border_thin')
+
+            cell = ws.cell(row=fila, column=8)
+            cell.value = item.pre_dias
+            self.set_style(cell=cell, font='font_10', alignment='left',
+                           border='border_thin')
+
+            fila += 1
+        historicos = self.datos_historicos(estacion, periodo, 'promedio')
+        max_historico = self.datos_historicos(estacion, periodo, 'maximo')
+        min_historico = self.datos_historicos(estacion, periodo, 'minimo')
+
+        fila = 9
+        for (item_his, item_max, item_min) in zip(historicos, max_historico, min_historico):
+            cell = ws.cell(row=fila, column=3)
+            cell.value = round(item_his, 1)
+            self.set_style(cell=cell, font='font_10', alignment='wrap',
+                           border='border_thin')
+
+            cell = ws.cell(row=fila, column=4)
+            cell.value = round(item_max, 1)
+            self.set_style(cell=cell, font='font_10', alignment='wrap',
+                           border='border_thin')
+
+            cell = ws.cell(row=fila, column=5)
+            cell.value = round(item_min, 1)
+            self.set_style(cell=cell, font='font_10', alignment='wrap',
+                           border='border_thin')
+            fila += 1
+
+
+
+
+
+    def grafico_excel(self, ws, estacion, periodo):
+        chart1 = BarChart()
+        chart1.type = "col"
+        chart1.style = 10
+        chart1.title = "Distribución temporal de Precipitación (mm)" + str(periodo)
+        chart1.y_axis.title = 'Precipitación (mm)'
+        chart1.x_axis.title = 'Meses'
+
+        data = Reference(ws, min_col=2, min_row=9, max_row=20, max_col=8)
+        cats = Reference(ws, min_col=1, min_row=9, max_row=20)
+        chart1.add_data(data, titles_from_data=True)
+        chart1.set_categories(cats)
+        chart1.shape = 4
+        ws.add_chart(chart1, "A10")
+
+
+
+
