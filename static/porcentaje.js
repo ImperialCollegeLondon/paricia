07299cb8ba8370a5bb143eabcaadbd27059a3aec
paul19pv <paul_19pv@hotmail.com>
2020-03-02 21:47:16 -0500

    Reporte porcentajes horarios

--- static/porcentaje.js
+++ static/porcentaje.js
@@ -18,4 +18,42 @@ $(document).ready(function() {
         $(".valor.error").first().closest("tr").get(0).scrollIntoView();
     });
 
+    $("#chk_valor").on("change", function(){
+        filtro = document.getElementById("chk_valor").value;
+
+        table = document.getElementById("tbl_data");
+        tr = table.getElementsByTagName("tr");
+        for (i = 0; i < tr.length; i++) {
+            td = tr[i].getElementsByTagName("td")[1];
+            if (td) {
+                txtValue = td.className;
+                if (txtValue.indexOf(filtro) > -1) {
+                    tr[i].style.display = "";
+                } else {
+                    tr[i].style.display = "none";
+                }
+            }
+
+        }
+    });
+
+    $("#chk_porcentaje").on("change", function(){
+        filtro = document.getElementById("chk_porcentaje").value;
+
+        table = document.getElementById("tbl_data");
+        tr = table.getElementsByTagName("tr");
+        for (i = 0; i < tr.length; i++) {
+            td = tr[i].getElementsByTagName("td")[2];
+            if (td) {
+                txtValue = td.className;
+                if (txtValue.indexOf(filtro) > -1) {
+                    tr[i].style.display = "";
+                } else {
+                    tr[i].style.display = "none";
+                }
+            }
+
+        }
+    });
+
 });
