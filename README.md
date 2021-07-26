99253049d65be4f2b2ede1ebed112ade4c36c5b4
Pablo Jácome <pxjacomex@gmail.com>
2021-07-26 00:29:39 -0500

    Se elimina todos los archivos de la rama para subir código desde cero

--- README.md
+++
@@ -1,3 +0,0 @@
-# sisimhea
-
-Sistema de gestión de datos hidroclimáticos para Iniciativa Regional de Monitoreo Hidrológico de Ecosistemas Andinos (iMHEA) basado en los sistemas desarrollados por FONAG y EPMAPS.
--- scripts/correcciones/formato_clasificacion__antiguo.sql
+++
@@ -1,6 +0,0 @@
-insert into formato_clasificacion(
-"cla_id","cla_valor","cla_maximo","cla_minimo","col_validador_valor","txt_validador_valor","col_validador_maximo","txt_validador_maximo","col_validador_minimo","txt_validador_minimo","acumular","incremental","resolucion","coma_decimal","for_id_id","var_id_id"
-) 
-VALUES (
-1,4,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,True,True,0.10,True,1,1
-);
