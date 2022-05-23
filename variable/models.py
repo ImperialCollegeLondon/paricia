316db8183170b58a7aee24da38ecdcbce481d05e
Daniel Davies <d.w.davies@ucl.ac.uk>
2022-05-23 13:23:25 +0100

    Translate models

--- variable/models.py
+++ variable/models.py
@@ -1,15 +1,15 @@
-# -*- coding: utf-8 -*-
-
-################################################################################################
-# Plataforma para la Iniciativa Regional de Monitoreo Hidrológico de Ecosistemas Andinos (iMHEA)
-# basada en los desarrollos realizados por:
+########################################################################################
+# Plataforma para la Iniciativa Regional de Monitoreo Hidrológico de Ecosistemas Andinos
+# (iMHEA)basada en los desarrollos realizados por:
 #     1) FONDO PARA LA PROTECCIÓN DEL AGUA (FONAG), Ecuador.
-#         Contacto: info@fonag.org.ec
-#     2) EMPRESA PÚBLICA METROPOLITANA DE AGUA POTABLE Y SANEAMIENTO DE QUITO (EPMAPS), Ecuador.
-#         Contacto: paramh2o@aguaquito.gob.ec
+#           Contacto: info@fonag.org.ec
+#     2) EMPRESA PÚBLICA METROPOLITANA DE AGUA POTABLE Y SANEAMIENTO DE QUITO (EPMAPS),
+#           Ecuador.
+#           Contacto: paramh2o@aguaquito.gob.ec
 #
-#  IMPORTANTE: Mantener o incluir esta cabecera con la mención de las instituciones creadoras,
-#              ya sea en uso total o parcial del código.
+#  IMPORTANTE: Mantener o incluir esta cabecera con la mención de las instituciones
+#  creadoras, ya sea en uso total o parcial del código.
+########################################################################################
 
 from __future__ import unicode_literals
 
@@ -20,31 +20,41 @@ from sensor.models import Sensor
 from station.models import Station
 
 
-class Unidad(models.Model):
-    uni_id = models.AutoField("Id", primary_key=True)
-    uni_nombre = models.CharField("Nombre", max_length=50)
-    uni_sigla = models.CharField("Sigla", max_length=10)
+class Unit(models.Model):
+    """
+    Unit of measurement with a name and an initialised form e.g. metres per second, m/s.
+    """
+
+    unit_id = models.AutoField("Id", primary_key=True)
+    name = models.CharField("Name", max_length=50)
+    initials = models.CharField("Initials", max_length=10)
 
     def __str__(self):
-        return str(self.uni_sigla)
+        return str(self.initials)
 
     def get_absolute_url(self):
-        return reverse("variable:unidad_detail", kwargs={"pk": self.pk})
+        return reverse("variable:unit_detail", kwargs={"pk": self.pk})
 
     class Meta:
-        ordering = ["uni_id"]
+        ordering = ["unit_id"]
 
 
 class Variable(models.Model):
-    var_id = models.AutoField("Id", primary_key=True)
-    var_codigo = models.CharField("Codigo", max_length=7)
-    var_nombre = models.CharField("Nombre", max_length=50)
-    # var_modelo = models.CharField("Modelo",max_length=50, blank=True, null=True)
-    uni_id = models.ForeignKey(
-        Unidad, models.SET_NULL, blank=True, null=True, verbose_name="Unidad"
+    """
+    A variable e.g. precipitation, wind speed, wind direction, soil moisture,
+    with an associated unit. Also has max, min... TODO: finish.
+    """
+
+    variable_id = models.AutoField("Id", primary_key=True)
+    variable_code = models.CharField("Code", max_length=7)
+    name = models.CharField("Name", max_length=50)
+    unit_id = models.ForeignKey(
+        Unit, models.SET_NULL, blank=True, null=True, verbose_name="Unidad"
     )
-    var_maximo = models.DecimalField("Maximo", max_digits=7, decimal_places=2)
-    var_minimo = models.DecimalField("Minimo", max_digits=7, decimal_places=2)
+    maximum = models.DecimalField("Maximum", max_digits=7, decimal_places=2)
+    minimum = models.DecimalField("Minimum", max_digits=7, decimal_places=2)
+    # TODO translate var_sos, var_err, var_estado and vacios if they're needed
+    # otherwise delete
     var_sos = models.DecimalField(
         "Incremento sospechoso", max_digits=7, decimal_places=2, null=True, blank=True
     )
@@ -55,52 +65,46 @@ class Variable(models.Model):
         "Sigmas (outliers)", max_digits=7, decimal_places=2, null=True, blank=True
     )
     var_estado = models.BooleanField("Estado", default=True)
-    es_acumulada = models.BooleanField(
-        "Acumulada (check)/ Promediada", default=True
-    )  # True: ACUMULADA, False: PROMEDIADA
-    reporte_automatico = models.BooleanField("Reporte Automático", default=True)
+    is_cumulative = models.BooleanField(
+        "Cumulative (True) or Averaged (False)", default=True
+    )
+    automatic_report = models.BooleanField("Automatic report", default=True)
     vacios = models.DecimalField(
         "Vacíos (%)", max_digits=4, decimal_places=1, null=True
     )
 
     def __str__(self):
-        return str(self.var_nombre)
+        return str(self.name)
 
     def get_absolute_url(self):
         return reverse("variable:variable_detail", kwargs={"pk": self.pk})
 
     class Meta:
-        ordering = ["var_id"]
+        ordering = ["variable_id"]
 
 
-class Control(models.Model):
-    con_id = models.AutoField("Id", primary_key=True)
-    var_id = models.ForeignKey(
+class SensorInstallation(models.Model):
+    """
+    Represents an installation of a Sensor at a Station, which measures a Variable.
+    Metadata for installation and finishing date, as well as state (active or not).
+    """
+
+    sensorinstallation_id = models.AutoField("Id", primary_key=True)
+    variable = models.ForeignKey(
         Variable, models.SET_NULL, blank=True, null=True, verbose_name="Variable"
     )
-    station_id = models.ForeignKey(
+    station = models.ForeignKey(
         Station, models.SET_NULL, blank=True, null=True, verbose_name="Station"
     )
-    sen_id = models.ForeignKey(
+    sensor = models.ForeignKey(
         Sensor, models.SET_NULL, blank=True, null=True, verbose_name="Sensor"
     )
-    con_fecha_ini = models.DateField("Fecha inicio")
-    con_fecha_fin = models.DateField("Fecha fin", blank=True, null=True)
-    con_estado = models.BooleanField("Activo", default=True)
+    start_date = models.DateField("Start date")
+    end_date = models.DateField("End date", blank=True, null=True)
+    state = models.BooleanField("Active", default=True)
 
     def get_absolute_url(self):
-        return reverse("variable:control_detail", kwargs={"pk": self.pk})
+        return reverse("variable:sensorinstallation_detail", kwargs={"pk": self.pk})
 
     class Meta:
-        ordering = ["station_id"]
-
-
-class CurvaDescarga(models.Model):
-    station = models.ForeignKey(
-        Station,
-        on_delete=models.CASCADE,
-        verbose_name="Station",
-        related_name="var_curvadescarga_station_id",
-    )
-    funcion = models.CharField("Función", max_length=200)
-    fecha = models.DateField("Fecha")
+        ordering = ["station"]
