e57e8ffed1af14015c4d80f0919c92a7e97c1e9b
Daniel Davies <d.w.davies@imperial.ac.uk>
2022-05-27 11:38:13 +0100

    Add variable tests

--- tests/variable/test_models.py
+++ tests/variable/test_models.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 
 
 class TestUnit(TestCase):
-    fixtures = ["variable_unit.json"]
+    fixtures = ["variable_unit.json", "variable_variable.json"]
 
     def test_units(self):
         from variable.models import Unit
@@ -12,3 +12,11 @@ class TestUnit(TestCase):
         unit = Unit.objects.get(unit_id=1)
         self.assertEqual(unit.name, "Kil\u00f3metros por Hora")
         self.assertEqual(unit.initials, "km/h")
+
+    def test_variables(self):
+        from variable.models import Variable
+
+        self.assertEqual(len(Variable.objects.get_queryset()), 30)
+        variable = Variable.objects.get(name="Temperatura ambiente")
+        self.assertEqual(variable.variable_code, "tai")
+        self.assertEqual(variable.unit.name, "Grados Cent\u00edgrados")
