149bd45f8128e5bee231bcad55970b801d2e36f2
Daniel Davies <d.w.davies@imperial.ac.uk>
2022-06-21 15:26:44 +0100

    Add some comments to current view

--- importing/views.py
+++ importing/views.py
@@ -130,8 +130,10 @@ class DataImportTempCreate(PermissionRequiredMixin, CreateView):
         file = copy.deepcopy(self.request.FILES["file"])
         matrix = preformat_matrix(file, form.cleaned_data["format"])
         del file
+        # Set start and end date based on cleaned data from the file
         form.instance.start_date = matrix.loc[0, "date"]
         form.instance.end_date = matrix.loc[matrix.shape[0] - 1, "date"]
+        # Set user from the request
         form.instance.user = self.request.user
         return super(DataImportTempCreate, self).form_valid(form)
 
@@ -142,6 +144,12 @@ class DataImportTempCreate(PermissionRequiredMixin, CreateView):
 
 
 class DataImportTempDetail(PermissionRequiredMixin, DetailView, FormView):
+    """
+    This view acts as the detail view of the DataImportTemp objects and creates
+    a new DataImportFull object. It also directly enters the measurement data into
+    the database using the save_temp_data_to_permanent function.
+    """
+
     permission_required = "importing.add_dataimportfull"  # TODO: is this right?
     model = DataImportTemp
     template_name = "importing/importingtemp_detail.html"
@@ -152,9 +160,13 @@ class DataImportTempDetail(PermissionRequiredMixin, DetailView, FormView):
         form = DataImportForm(request.POST or None)
         if form.is_valid():
             if request.POST["action"] == "confirm":
+                # Get the pk of the DataImportTemp object
                 imp_id__temp = kwargs.get("pk")
+                # Save the actual measurement data
                 imp_id = save_temp_data_to_permanent(imp_id__temp, form)
+                # Get the DataImportFull object as returned by the above function
                 data_import = DataImportFull.objects.get(imp_id=imp_id)
+                # Get the variables that are saved in this dataset
                 classifications = data_import.format.classification_set.all()
                 variables = []
                 for classification in classifications:
