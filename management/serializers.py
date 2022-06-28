85442c0cd055135bb88bbc02803b4f4b7e3a24ba
Daniel Davies <d.w.davies@ucl.ac.uk>
2022-06-28 14:25:39 +0100

    add note to users serializers

--- management/serializers.py
+++ management/serializers.py
@@ -6,6 +6,8 @@ from .models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    # TODO this needs thinking about - how do we want to handle
+    # serialization of users
     data_imports_temp = serializers.PrimaryKeyRelatedField(
         many=True, queryset=DataImportTemp.objects.all()
     )
