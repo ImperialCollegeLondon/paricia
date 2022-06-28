4f9d52ff929f4ac517b7484215991d96c6db0c26
Daniel Davies <d.w.davies@ucl.ac.uk>
2022-06-28 14:14:03 +0100

    Add user views

--- management/views.py
+++ management/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics
+
+from .models import User
+from .serializers import UserSerializer
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
 
 # Create your views here.
