e8feb3188611e6aed7f0437d028f2e16e3a24094
Daniel Davies <d.w.davies@ucl.ac.uk>
2022-06-28 12:48:29 +0100

    Remove unused functions form station.views

--- station/views.py
+++ station/views.py
@@ -11,23 +11,10 @@
 #  creadoras, ya sea en uso total o parcial del código.
 ########################################################################################
 
-from __future__ import unicode_literals
-
-from django.contrib.auth.decorators import permission_required
-from django.contrib.auth.mixins import PermissionRequiredMixin
-from django.db.models import Max, Min, Q, Value
-from django.db.models.functions import Replace
-from django.http import JsonResponse
-from django.urls import reverse_lazy
-from django.views.generic import TemplateView
-from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from rest_framework import generics
 
 import station.models as stn
 import station.serializers as serializers
-from station.functions import excel_station
-from utilities.functions import modelo_a_tabla_html
 
 
 class CountryList(generics.ListCreateAPIView):
@@ -118,88 +105,3 @@ class StationList(generics.ListCreateAPIView):
 class StationDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = stn.Station.objects.all()
     serializer_class = serializers.StationSerializer
-
-
-############################################################################
-
-
-@permission_required("station.view_station")
-def station_query(request):
-    """
-    Station query view to get the station based on various possible
-    query terms.
-    NOTE: Lots of bare except statements and it's unclear when/why this
-          would be used. Do we need it?
-    """
-    variable_id = place_id = basin_id = station_type_id = None
-    filter_query = Q()
-
-    try:
-        variable_id = int(request.GET.get("variable_id", None))
-    except Exception as e:
-        pass
-
-    try:
-        place_id = int(request.GET.get("place_id", None))
-    except Exception as e:
-        pass
-
-    try:
-        basin_id = int(request.GET.get("basin_id", None))
-    except Exception as e:
-        pass
-
-    try:
-        station_type_id = int(request.GET.get("station_type_id", None))
-    except Exception as e:
-        pass
-
-    # NOTE: cruce has been (for now at least) removed so this query is broken
-    if variable_id:
-        filter_query &= Q(cruce__var_id_id=variable_id)
-    if place_id:
-        filter_query &= Q(placebasin__place_id=place_id)
-    if basin_id:
-        filter_query &= Q(placebasin__basin_id=basin_id)
-    if station_type_id:
-        filter_query &= Q(station_type_id=station_type_id)
-
-    stations = stn.Station.objects.filter(filter_query)
-
-    imagen = None
-    if basin_id:
-        imagen = stn.Basin.objects.get(id=basin_id).imagen
-    elif place_id:
-        imagen = stn.Place.objects.get(id=place_id).imagen
-
-    try:
-        imagen_url = imagen.url
-    except:
-        imagen_url = ""
-
-    lista = {"stations": {}, "imagen": imagen_url}
-
-    for row in stations:
-        # print(row.station_longitude)
-        # print(row.station_altitude)
-        lista["stations"][row.station_id] = row.station_code
-    return JsonResponse(lista)
-
-
-def list_year(request, station, var):
-    """
-    List data for a number of whole calendar years.
-    """
-
-    model = "Var" + str(var) + "Measurement"
-    model = globals()[model]
-    valid = model.objects.filter(station_id__exact=station).aggregate(
-        Max("date"), Min("date")
-    )
-    if valid["date__max"] is not None:
-        valid["date__max"] = valid["date__max"].year
-        valid["date__min"] = valid["date__min"].year
-        dates = list(range(valid["date__min"], valid["date__max"] + 1))
-    else:
-        dates = ["No data"]
-    return JsonResponse(dates, safe=False)
