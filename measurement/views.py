e6e63c902017fc069f5b18e807030f72e6191e7e
Daniel Davies <d.w.davies@imperial.ac.uk>
2022-07-15 15:42:06 +0100

    Add API list and filters

--- measurement/views.py
+++ measurement/views.py
@@ -27,15 +27,282 @@ import measurement.models as meas
 import measurement.serializers as serializers
 from measurement.models import DischargeCurve, LevelFunction
 
-from .filters import MeasurementFilter, MeasurementFilterDepth
+from .filters import (
+    DischargeCurveFilter,
+    LevelFunctionFilter,
+    MeasurementFilter,
+    MeasurementFilterDepth,
+    PolarWindFilter,
+)
 from .forms import LevelFunctionForm
 from .functions import level_function_table
 
 
-class FlowList(generics.ListAPIView):
+class PolarWindList(generics.ListAPIView):
+    """
+    List all measurements of Polar Wind.
+    """
+
+    queryset = meas.PolarWind.objects.all()
+    serializer_class = serializers.PolarWindSerializer
+    filterset_class = PolarWindFilter
+
+
+class DischargeCurveList(generics.ListAPIView):
+    """
+    List all measurements of Discharge Curve.
+    """
+
+    queryset = meas.DischargeCurve.objects.all()
+    serializer_class = serializers.DischargeCurveSerializer
+    filterset_class = DischargeCurveFilter
+
+
+class LevelFunctionList(generics.ListAPIView):
+    """
+    List all measurements of Level Function.
+    """
+
+    queryset = meas.LevelFunction.objects.all()
+    serializer_class = serializers.LevelFunctionSerializer
+    filterset_class = LevelFunctionFilter
+
+
+##############################################################
+
+
+class MeasurementListBase(generics.ListAPIView):
+    """
+    Base class for the measurement list views that all use the
+    MeasurementFilter class to filter the results.
+    """
+
+    filterset_class = MeasurementFilter
+
+
+class MeasurementDepthListBase(generics.ListAPIView):
+    """
+    Base class for the measurement list views that all use the
+    MeasurementFilterDepth class to filter the results.
+    """
+
+    filterset_class = MeasurementFilterDepth
+
+
+class PrecipitationList(MeasurementListBase):
+    """
+    List all measurements of Precipitation.
+    """
+
+    queryset = meas.Precipitation.objects.all()
+    serializer_class = serializers.PrecipitationSerializer
+
+
+class AirTemperatureList(MeasurementListBase):
+    """
+    List all measurements of Air Temperature.
+    """
+
+    queryset = meas.AirTemperature.objects.all()
+    serializer_class = serializers.AirTemperatureSerializer
+
+
+class HumidityList(MeasurementListBase):
+    """
+    List all measurements of Humidity.
+    """
+
+    queryset = meas.Humidity.objects.all()
+    serializer_class = serializers.HumiditySerializer
+
+
+class WindVelocityList(MeasurementListBase):
+    """
+    List all measurements of Wind Velocity.
+    """
+
+    queryset = meas.WindVelocity.objects.all()
+    serializer_class = serializers.WindVelocitySerializer
+
+
+class WindDirectionList(MeasurementListBase):
+    """
+    List all measurements of Wind Direction.
+    """
+
+    queryset = meas.WindDirection.objects.all()
+    serializer_class = serializers.WindDirectionSerializer
+
+
+class SoilMoistureList(MeasurementListBase):
+    """
+    List all measurements of Soil Moisture.
+    """
+
+    queryset = meas.SoilMoisture.objects.all()
+    serializer_class = serializers.SoilMoistureSerializer
+
+
+class SolarRadiationList(MeasurementListBase):
+    """
+    List all measurements of Solar Radiation.
+    """
+
+    queryset = meas.SolarRadiation.objects.all()
+    serializer_class = serializers.SolarRadiationSerializer
+
+
+class AtmposphericPressureList(MeasurementListBase):
+    """
+    List all measurements of Atmospheric Pressure.
+    """
+
+    queryset = meas.AtmosphericPressure.objects.all()
+    serializer_class = serializers.AtmosphericPressureSerializer
+
+
+class WaterTemperatureList(MeasurementListBase):
+    """
+    List all measurements of Water Temperature.
+    """
+
+    queryset = meas.WaterTemperature.objects.all()
+    serializer_class = serializers.WaterTemperatureSerializer
+
+
+class FlowList(MeasurementListBase):
+    """
+    List all measurements of Flow.
+    """
+
     queryset = meas.Flow.objects.all()
     serializer_class = serializers.FlowSerializer
-    filterset_class = MeasurementFilter
+
+
+class WaterLevelList(MeasurementListBase):
+    """
+    List all measurements of Water Level.
+    """
+
+    queryset = meas.WaterLevel.objects.all()
+    serializer_class = serializers.WaterLevelSerializer
+
+
+class BatteryVoltageList(MeasurementListBase):
+    """
+    List all measurements of Battery Voltage.
+    """
+
+    queryset = meas.BatteryVoltage.objects.all()
+    serializer_class = serializers.BatteryVoltageSerializer
+
+
+class FlowManualList(MeasurementListBase):
+    """
+    List all measurements of Flow Manual.
+    """
+
+    queryset = meas.FlowManual.objects.all()
+    serializer_class = serializers.FlowManualSerializer
+
+
+class StripLevelReadingList(MeasurementListBase):
+    """
+    List all measurements of Strip Level Reading.
+    """
+
+    queryset = meas.StripLevelReading.objects.all()
+    serializer_class = serializers.StripLevelReadingSerializer
+
+
+class SoilTemperatureList(MeasurementListBase):
+    """
+    List all measurements of Soil Temperature.
+    """
+
+    queryset = meas.SoilTemperature.objects.all()
+    serializer_class = serializers.SoilTemperatureSerializer
+
+
+class IndirectRadiationList(MeasurementListBase):
+    """
+    List all measurements of Indirect Radiation.
+    """
+
+    queryset = meas.IndirectRadiation.objects.all()
+    serializer_class = serializers.IndirectRadiationSerializer
+
+
+class WaterTemperatureDepthList(MeasurementDepthListBase):
+    """
+    List all measurements of Water Temperature Depth.
+    """
+
+    queryset = meas.WaterTemperatureDepth.objects.all()
+    serializer_class = serializers.WaterTemperatureDepthSerializer
+
+
+class WaterAcidityDepthList(MeasurementDepthListBase):
+    """
+    List all measurements of Water Acidity Depth.
+    """
+
+    queryset = meas.WaterAcidityDepth.objects.all()
+    serializer_class = serializers.WaterAcidityDepthSerializer
+
+
+class RedoxPotentialDepthList(MeasurementDepthListBase):
+    """
+    List all measurements of Redox Potential Depth.
+    """
+
+    queryset = meas.RedoxPotentialDepth.objects.all()
+    serializer_class = serializers.RedoxPotentialDepthSerializer
+
+
+class WaterTurbidityDepthList(MeasurementDepthListBase):
+    """
+    List all measurements of Water Turbidity Depth.
+    """
+
+    queryset = meas.WaterTurbidityDepth.objects.all()
+    serializer_class = serializers.WaterTurbidityDepthSerializer
+
+
+class ChlorineConcentrationDepthList(MeasurementDepthListBase):
+    """
+    List all measurements of Chlorine Concentration Depth.
+    """
+
+    queryset = meas.ChlorineConcentrationDepth.objects.all()
+    serializer_class = serializers.ChlorineConcentrationDepthSerializer
+
+
+class OxygenConcentrationDepthList(MeasurementDepthListBase):
+    """
+    List all measurements of Oxygen Concentration Depth.
+    """
+
+    queryset = meas.OxygenConcentrationDepth.objects.all()
+    serializer_class = serializers.OxygenConcentrationDepthSerializer
+
+
+class PercentageOxygenConcentrationDepthList(MeasurementDepthListBase):
+    """
+    List all measurements of Percentage Oxygen Concentration Depth.
+    """
+
+    queryset = meas.PercentageOxygenConcentrationDepth.objects.all()
+    serializer_class = serializers.PercentageOxygenConcentrationDepthSerializer
+
+
+class PhycocyaninDepthList(MeasurementDepthListBase):
+    """
+    List all measurements of Phycocyanin Depth.
+    """
+
+    queryset = meas.PhycocyaninDepth.objects.all()
+    serializer_class = serializers.PhycocyaninDepthSerializer
 
 
 ########################################################################################
