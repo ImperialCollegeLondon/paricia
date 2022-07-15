164f5a9c04f9cdbeed92d856ed91e6a0c4d0ba76
Daniel Davies <d.w.davies@imperial.ac.uk>
2022-07-15 16:36:47 +0100

    start on api schema

--- djangomain/settings.py
+++ djangomain/settings.py
@@ -70,6 +70,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "management",
     "crispy_forms",
+    "rest_framework_swagger",
 ]
 
 MIDDLEWARE = [
@@ -199,4 +200,5 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 100,
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
+    "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
 }
