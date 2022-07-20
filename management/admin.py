fcf49229bb4ddbcaca115007e903b866c942be0a
Daniel Davies <d.w.davies@ucl.ac.uk>
2022-07-20 13:15:07 +0100

    custom admin

--- management/admin.py
+++ management/admin.py
@@ -1,3 +1,81 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from .models import User
+
+
+class CustomUserAdmin(UserAdmin):
+    """A slightly more restrictive user admin page."""
+
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (
+            _("Personal info"),
+            {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                    "email",
+                )
+            },
+        ),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "password1", "password2"),
+            },
+        ),
+    )
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        is_superuser = request.user.is_superuser
+        disabled_fields = set()
+
+        # Prevent changing permissions without using groups
+        if not is_superuser:
+            disabled_fields |= {
+                "is_superuser",
+                "user_permissions",
+            }
+        # Prevent users changing own permissions or those of AnonymousUser
+        if (
+            not is_superuser
+            and obj is not None
+            and (
+                obj == request.user or obj == User.objects.get(username="AnonymousUser")
+            )
+        ):
+            disabled_fields |= {
+                "is_staff",
+                "is_superuser",
+                "groups",
+                "user_permissions",
+            }
+
+        for f in disabled_fields:
+            if f in form.base_fields:
+                form.base_fields[f].disabled = True
+
+        return form
+
+
+admin.site.register(User, CustomUserAdmin)
