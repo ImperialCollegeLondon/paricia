9d46408f5e58d0d20a6d3e519601727c922d3fe6
Daniel Davies <d.w.davies@ucl.ac.uk>
2022-07-20 11:16:45 +0100

    Update api view

--- djangomain/views.py
+++ djangomain/views.py
@@ -10,10 +10,10 @@ class HomePageView(TemplateView):
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Snippets API",
+        title="Paricia API",
         default_version="v1",
-        description="Test description",
-        terms_of_service="https://www.google.com/policies/terms/",
+        description="API for the Paricia project",
+        terms_of_service="https://github.com/ImperialCollegeLondon/paricia",
         contact=openapi.Contact(email="contact@snippets.local"),
         license=openapi.License(name="BSD License"),
     ),
