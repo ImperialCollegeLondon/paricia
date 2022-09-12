ef409e231b774e5be32393aaa11874ff6ce65df2
Daniel Davies <d.w.davies@ucl.ac.uk>
2022-09-12 14:33:44 +0100

    fix formatting

--- djangomain/renderers.py
+++ djangomain/renderers.py
@@ -1,5 +1,6 @@
 from rest_framework.renderers import BrowsableAPIRenderer
 
+
 class TableAPIRenderer(BrowsableAPIRenderer):
-    format = 'table'
-    template = 'table.html'
+    format = "table"
+    template = "table.html"
