9443b125d6d4da2b35d94e22818f94de4fa08b9a
Diego Alonso Alvarez <d.alonso-alvarez@imperial.ac.uk>
2024-01-17 15:30:00 +0000

    Add measurement to admin

--- measurement/admin.py
+++ measurement/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Report
+from .models import Measurement, Report
 
 admin.site.site_header = "Paricia Administration - Measurements"
 
@@ -12,5 +12,17 @@ class MeasurementBaseAdmin(admin.ModelAdmin):
 
 @admin.register(Report)
 class ReportAdmin(MeasurementBaseAdmin):
-    list_display = ["report_type"] + MeasurementBaseAdmin.list_display
+    list_display = ["id", "report_type"] + MeasurementBaseAdmin.list_display[1:]
     list_filter = ["report_type"] + MeasurementBaseAdmin.list_filter
+
+
+@admin.register(Measurement)
+class MeasurementAdmin(MeasurementBaseAdmin):
+    list_display = [
+        "id",
+        "is_validated",
+        "is_active",
+        "overwritten",
+    ] + MeasurementBaseAdmin.list_display[1:]
+    list_filter = ["is_validated", "is_active"] + MeasurementBaseAdmin.list_filter
+    readonly_fields = [r for r in dir(Measurement) if r.startswith("raw_")]
