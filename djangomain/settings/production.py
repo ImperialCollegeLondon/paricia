fc08953cd4a5ed10c20da3c3b5323569d37fe006
Diego Alonso Álvarez <d.alonso-alvarez@imperial.ac.uk>
2024-02-16 16:39:02 +0000

    Fix wrong allowed host

--- djangomain/settings/production.py
+++ djangomain/settings/production.py
@@ -3,6 +3,6 @@ import os
 from .settings import *  # noqa: F401, F403
 
 DEBUG = False
-ALLOWED_HOSTS = ["172.31.1431"]
+ALLOWED_HOSTS = ["172.31.14.31"]
 SECRET_KEY = os.environ["SECRET_KEY"]
 DATABASES["default"]["PASSWORD"] = os.environ["POSTGRES_PASSWORD"]  # noqa: F405
