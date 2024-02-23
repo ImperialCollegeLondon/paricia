51aa859fbe7b52728ae51d31eebb9b7c20769408
Diego Alonso Álvarez <d.alonso-alvarez@imperial.ac.uk>
2024-02-23 08:58:40 +0000

    Add view and serialiser for measurements and reports

--- measurement/views.py
+++ measurement/views.py
@@ -38,6 +38,74 @@ from .forms import LevelFunctionForm
 from .functions import level_function_table
 
 
+class MeasurementList(generics.ListAPIView):
+    """
+    List measurements of all types.
+
+    This view is used to retrieve the measurement records filtering by station,
+    variable, start_time, end_time, is_validated and is_active. If no parameters
+    are provided, an empty queryset is returned.
+    """
+
+    model = meas.Measurement
+    serializer_class = serializers.MeasurementSerializer
+
+    def get_queryset(self):
+        """Custom queryset to filter the results."""
+        params = self.request.query_params
+        options = {}
+        if params.get("station"):
+            options["station__station_id"] = params.get("station_id")
+        if params.get("variable"):
+            options["variable__variable_id"] = params.get("variable_id")
+        if params.get("start_time"):
+            options["time__gte"] = params.get("start_time")
+        if params.get("end_time"):
+            options["time__lte"] = params.get("end_time")
+        if params.get("is_validated"):
+            options["is_validated"] = params.get("is_validated")
+        if params.get("is_active"):
+            options["is_active"] = params.get("is_active")
+
+        if not options:
+            return self.model.objects.none()
+
+        return self.model.objects.filter(**options)
+
+
+class ReportList(generics.ListAPIView):
+    """
+    List all measurements of Report.
+
+    This view is used to retrieve the results filtering by station, variable,
+    report_type, start_time, and end_time. If no parameters are provided, an empty
+    queryset is returned.
+    """
+
+    model = meas.Report
+    serializer_class = serializers.ReportSerializer
+
+    def get_queryset(self):
+        """Custom queryset to filter the results."""
+        params = self.request.query_params
+        options = {}
+        if params.get("station"):
+            options["station__station_id"] = params.get("station_id")
+        if params.get("variable"):
+            options["variable__variable_id"] = params.get("variable_id")
+        if params.get("report_type"):
+            options["report_type"] = params.get("report_type")
+        if params.get("start_time"):
+            options["time__gte"] = params.get("start_time")
+        if params.get("end_time"):
+            options["time__lte"] = params.get("end_time")
+
+        if not options:
+            return self.model.objects.none()
+
+        return self.model.objects.filter(**options)
+
+
 class PolarWindList(generics.ListAPIView):
     """
     List all measurements of Polar Wind.
