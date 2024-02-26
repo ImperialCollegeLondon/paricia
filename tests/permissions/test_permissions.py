5fe060950c0079c46cce027f61d5ed5e9480d073
Tom Bland <tom_bland@hotmail.co.uk>
2024-02-26 20:13:59 +0000

    Better tests (currently failing)

--- tests/permissions/test_permissions.py
+++ tests/permissions/test_permissions.py
@@ -1,24 +1,109 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
-from guardian.shortcuts import get_perms
 
+from measurement.models import Measurement
 from station.models import Station
 
 User = get_user_model()
 
 
-class StationPermissionsTest(TestCase):
-    def setUp(self):
-        self.user1 = User.objects.create_user(username="user1", password="password")
-        self.user2 = User.objects.create_user(username="user2", password="password")
-        self.station = Station.objects.create(owner=self.user1)
+class BasePermissionsTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+        cls.user_owner = User.objects.create_user(
+            username="user_owner", password="password"
+        )
+        cls.user_other = User.objects.create_user(
+            username="user_other", password="password"
+        )
+        cls.user_inactive = User.objects.create_user(
+            username="user_inactive", password="password", is_active=False
+        )
 
-    def test_view_permission_for_all_users(self):
-        self.assertTrue(self.user1.has_perm("station.view_station", self.station))
-        self.assertTrue(self.user2.has_perm("station.view_station", self.station))
 
-    def test_change_and_delete_permissions(self):
-        self.assertTrue(self.user1.has_perm("station.change_station", self.station))
-        self.assertTrue(self.user1.has_perm("station.delete_station", self.station))
-        self.assertFalse(self.user2.has_perm("station.change_station", self.station))
-        self.assertFalse(self.user2.has_perm("station.delete_station", self.station))
+class StationPermissionsTest(BasePermissionsTest):
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+        cls.station = Station.objects.create(owner=cls.user_owner)
+
+    def test_change_permissions(self):
+        """Test that only the owner can change the station."""
+        self.assert_perm(self.user_owner, "change", self.station, True)
+        self.assert_perm(self.user_other, "change", self.station, False)
+        self.assert_perm(self.user_inactive, "change", self.station, False)
+
+    def test_delete_permissions(self):
+        """Test that only the owner can delete the station."""
+        self.assert_perm(self.user_owner, "delete", self.station, True)
+        self.assert_perm(self.user_other, "delete", self.station, False)
+        self.assert_perm(self.user_inactive, "delete", self.station, False)
+
+    def test_view_permissions(self):
+        """Test that all users can view the station."""
+        self.assert_perm(self.user_owner, "view", self.station, True)
+        self.assert_perm(self.user_other, "view", self.station, True)
+        self.assert_perm(self.user_inactive, "view", self.station, True)
+
+    def assert_perm(self, user, perm, obj, expected):
+        """Helper function to assert permissions."""
+        if expected:
+            self.assertTrue(user.has_perm(f"station.{perm}_station", obj))
+        else:
+            self.assertFalse(user.has_perm(f"station.{perm}_station", obj))
+
+
+class MeasurementPermissionsTest(BasePermissionsTest):
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+        cls.station_private = Station.objects.create(
+            owner=cls.user_owner, permissions_level="private"
+        )
+        cls.station_public = Station.objects.create(
+            owner=cls.user_owner, permissions_level="public"
+        )
+        cls.station_internal = Station.objects.create(
+            owner=cls.user_owner, permissions_level="internal"
+        )
+        cls.meas_private = Measurement.objects.create(station=cls.station_private)
+        cls.meas_public = Measurement.objects.create(station=cls.station_public)
+        cls.meas_internal = Measurement.objects.create(station=cls.station_internal)
+
+    def test_change_permissions(self):
+        """Test that only the owner can change the measurement."""
+        self.assert_perm(self.user_owner, "change", self.meas_private, True)
+        self.assert_perm(self.user_other, "change", self.meas_private, False)
+        self.assert_perm(self.user_inactive, "change", self.meas_internal, False)
+
+    def test_delete_permissions(self):
+        """Test that only the owner can delete the measurement."""
+        self.assert_perm(self.user_owner, "delete", self.meas_private, True)
+        self.assert_perm(self.user_other, "delete", self.meas_private, False)
+        self.assert_perm(self.user_inactive, "delete", self.meas_internal, False)
+
+    def test_view_permissions_private(self):
+        """Test that only the owner can view the private measurement."""
+        self.assert_perm(self.user_owner, "view", self.meas_private, True)
+        self.assert_perm(self.user_other, "view", self.meas_private, False)
+        self.assert_perm(self.user_inactive, "view", self.meas_private, False)
+
+    def test_view_permissions_internal(self):
+        """Test that only active users can view the internal measurement."""
+        self.assert_perm(self.user_owner, "view", self.meas_internal, True)
+        self.assert_perm(self.user_other, "view", self.meas_internal, True)
+        self.assert_perm(self.user_inactive, "view", self.meas_internal, False)
+
+    def test_view_permissions_public(self):
+        """Test that all users can view the public measurement."""
+        self.assert_perm(self.user_owner, "view", self.meas_public, True)
+        self.assert_perm(self.user_other, "view", self.meas_public, True)
+        self.assert_perm(self.user_inactive, "view", self.meas_public, True)
+
+    def assert_perm(self, user, perm, obj, expected):
+        """Helper function to assert permissions."""
+        if expected:
+            self.assertTrue(user.has_perm(f"measurement.{perm}_measurement", obj))
+        else:
+            self.assertFalse(user.has_perm(f"measurement.{perm}_measurement", obj))
