d3b8535e3d66aaca6f60c33af84a065da209ef89
Tom Bland <tom_bland@hotmail.co.uk>
2024-02-27 17:10:00 +0000

    Show only available options

--- station/admin.py
+++ station/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.core.exceptions import PermissionDenied
+from django.db.models import Q
 from guardian.admin import GuardedModelAdmin
 from guardian.shortcuts import get_perms
 
@@ -30,6 +32,17 @@ class StationAdmin(GuardedModelAdmin):
     ]
     list_filter = ["station_type", "country", "region", "ecosystem", "institution"]
 
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name in [
+            "station_type",
+            "country",
+            "region",
+            "ecosystem",
+            "institution",
+        ]:
+            kwargs["queryset"] = get_queryset(db_field.related_model, request.user)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
     def has_add_permission(self, request):
         return request.user.is_authenticated
 
@@ -47,3 +60,17 @@ class StationAdmin(GuardedModelAdmin):
         if obj is not None:
             return "view_station" in get_perms(request.user, obj)
         return True
+
+    def save_model(self, request, obj, form, change):
+        fields = ["station_type", "country", "region", "ecosystem", "institution"]
+        for field in fields:
+            owner = getattr(obj, field).owner
+            perm_level = getattr(obj, field).permissions_level
+            if owner != request.user and perm_level == "Private":
+                raise PermissionDenied(f"Private {field}: Only owner can use.")
+
+        super().save_model(request, obj, form, change)
+
+
+def get_queryset(model, user):
+    return model.objects.filter(Q(owner=user) | Q(permissions_level="Public"))
