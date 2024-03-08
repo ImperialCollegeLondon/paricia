6716e9c13d13194bb30979dbbc05083f1e0a03ce
Tom Bland <tom_bland@hotmail.co.uk>
2024-03-08 14:14:20 +0000

    Remove duplicated code

--- station/signals/handlers.py
+++ station/signals/handlers.py
@@ -1,7 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from guardian.shortcuts import assign_perm
 
 from station.models import (
     Basin,
@@ -28,25 +27,3 @@ User = get_user_model()
 def set_permissions(sender, instance, **kwargs):
     """Set object-level permissions."""
     instance.set_permissions()
-
-    # Assign view_measurements permission for Station objects
-    if sender == Station:
-        permissions_users = {
-            "public": User.objects.all(),
-            "internal": User.objects.filter(is_active=True),
-            "private": [instance.owner] if instance.owner else [],
-        }
-        users = permissions_users[instance.permissions_level]
-        for user in users:
-            assign_perm("view_measurements", user, instance)
-
-
-def _get_perm_codenames(model):
-    """Helper function to get delete, change and view permission codenames for a
-    given model.
-    """
-    return (
-        f"delete_{model._meta.model_name}",
-        f"change_{model._meta.model_name}",
-        f"view_{model._meta.model_name}",
-    )
