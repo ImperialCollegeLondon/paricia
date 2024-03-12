f39eb2abf646bd48a269f4e5c1e092ab0481b616
Tom Bland <tom_bland@hotmail.co.uk>
2024-03-12 14:29:05 +0000

    View permissions for internal objects

--- management/models.py
+++ management/models.py
@@ -31,7 +31,7 @@ class PermissionsBase(models.Model):
         delete, change, view = _get_perm_codenames(self.__class__)
 
         # Assign view permissions for all users
-        if self.permissions_level == "public":
+        if self.permissions_level in ["public", "internal"]:
             assign_perm(view, User.objects.all(), self)
         elif self.permissions_level == "private" and self.owner:
             assign_perm(view, self.owner, self)
