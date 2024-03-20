cbf71990cda2170ca28f6def584a4eef91df5c0c
Tom Bland <tom_bland@hotmail.co.uk>
2024-03-20 14:16:54 +0000

    Admin for PlaceBasin

--- station/signals/handlers.py
+++ station/signals/handlers.py
@@ -9,6 +9,7 @@ from station.models import (
     Ecosystem,
     Institution,
     Place,
+    PlaceBasin,
     Region,
     Station,
     StationType,
@@ -24,6 +25,7 @@ User = get_user_model()
 @receiver(post_save, sender=StationType)
 @receiver(post_save, sender=Place)
 @receiver(post_save, sender=Basin)
+@receiver(post_save, sender=PlaceBasin)
 @receiver(post_save, sender=Station)
 @receiver(post_save, sender=DeltaT)
 def set_object_permissions(sender, instance, **kwargs):
@@ -42,6 +44,7 @@ def set_model_permissions(sender, **kwargs):
         StationType,
         Place,
         Basin,
+        PlaceBasin,
         Station,
         DeltaT,
     ]:
