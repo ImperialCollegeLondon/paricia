90f2ecca9cf6e32f6bded395e5e3c5cac83a8e84
Diego Alonso Alvarez <d.alonso-alvarez@imperial.ac.uk>
2024-09-24 18:34:31 +0100

    :bug: Check right permissions before showing objects. Update docstrings.

--- importing/urls.py
+++ importing/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from .views import data_import_list, data_import_view
+from .views import data_import_detail_view, data_import_list
 
 app_name = "import"
 urlpatterns = [
     path("", data_import_list, name="data_import_list"),
-    path("<int:pk>/", data_import_view, name="data_import_detail"),
+    path("<int:pk>/", data_import_detail_view, name="data_import_detail"),
 ]
--- importing/views.py
+++ importing/views.py
@@ -3,16 +3,30 @@ from contextlib import suppress
 from django import forms
 from django.contrib.auth.decorators import login_required
 from django.db.models import ForeignKey, Model
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import get_object_or_404, render
 from django_tables2 import RequestConfig
+from guardian.shortcuts import get_objects_for_user
 
 from .models import DataImport
 from .tables import DataImportTable
 
 
 @login_required
-def data_import_list(request):
-    table = DataImportTable(DataImport.objects.all())
+def data_import_list(request: HttpRequest) -> HttpResponse:
+    """View to list the data imports.
+
+    This view lists the data imports that the user has permission to view.
+
+    Args:
+        request (HttpRequest): Object representing the request.
+
+    Returns:
+        HttpResponse: Response object with the rendered template.
+    """
+    table = DataImportTable(
+        get_objects_for_user(request.user, "importing.view_dataimport")
+    )
     RequestConfig(request, paginate={"per_page": 10}).configure(table)
     return render(
         request,
@@ -22,6 +36,17 @@ def data_import_list(request):
 
 
 def model_to_dict(instance: Model) -> dict:
+    """Convert a model instance to a dictionary.
+
+    For ForeignKey fields, the related model instance is converted to a string, so that
+    a meaningful representation is shown in the dictionary instead of the primary key.
+
+    Args:
+        instance (Model): Model instance to convert.
+
+    Returns:
+        dict: Dictionary with the model instance data.
+    """
     data = {}
     for field in instance._meta.get_fields():
         data[field.name] = field.value_from_object(instance)
@@ -34,14 +59,30 @@ def model_to_dict(instance: Model) -> dict:
 
 
 @login_required
-def data_import_view(request, pk):
+def data_import_detail_view(request: HttpRequest, pk: int) -> HttpResponse:
+    """View to view a data import.
+
+    This view shows the details of a data import. The user must have permission to view
+    the data import object, otherwise a 403 error is returned.
+
+    Args:
+        request (HttpRequest): Object representing the request.
+        pk (int): Primary key of the data import object.
+
+    Returns:
+        HttpResponse: Response object with the rendered template.
+    """
+
     class DataImportForm(forms.ModelForm):
         class Meta:
             model = DataImport
             fields = "__all__"
 
-    model = get_object_or_404(DataImport, pk=pk)
-    form = DataImportForm(data=model_to_dict(model), instance=model)
+    instance = get_object_or_404(DataImport, pk=pk)
+    if not request.user.has_perm("importing.view_dataimport", instance):
+        return render(request, "403.html", status=403)
+
+    form = DataImportForm(data=model_to_dict(instance))
     return render(
         request,
         "object_detail.html",
