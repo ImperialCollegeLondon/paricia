f4d9ca0d5c156da1c92ab6d0c817f15ced8cc57f
Diego Alonso Alvarez <d.alonso-alvarez@imperial.ac.uk>
2024-09-24 18:07:20 +0100

    :recycle: Handle ForeignKeys correctly in detail view.

--- importing/views.py
+++ importing/views.py
@@ -1,5 +1,8 @@
+from contextlib import suppress
+
 from django import forms
 from django.contrib.auth.decorators import login_required
+from django.db.models import ForeignKey, Model
 from django.shortcuts import get_object_or_404, render
 from django_tables2 import RequestConfig
 
@@ -18,6 +21,18 @@ def data_import_list(request):
     )
 
 
+def model_to_dict(instance: Model) -> dict:
+    data = {}
+    for field in instance._meta.get_fields():
+        data[field.name] = field.value_from_object(instance)
+        if isinstance(field, ForeignKey):
+            with suppress(field.related_model.DoesNotExist):
+                data[field.name] = str(
+                    field.related_model.objects.get(pk=data[field.name])
+                )
+    return data
+
+
 @login_required
 def data_import_view(request, pk):
     class DataImportForm(forms.ModelForm):
@@ -26,7 +41,7 @@ def data_import_view(request, pk):
             fields = "__all__"
 
     model = get_object_or_404(DataImport, pk=pk)
-    form = DataImportForm(instance=model)
+    form = DataImportForm(data=model_to_dict(model), instance=model)
     return render(
         request,
         "object_detail.html",
