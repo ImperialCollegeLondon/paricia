282c40c5272d5a915bb217322ba791b5256ef35c
Diego Alonso Alvarez <d.alonso-alvarez@imperial.ac.uk>
2024-09-25 21:01:55 +0100

    :sparkles: Add formatting filtering for DataImport.

--- importing/filters.py
+++ importing/filters.py
@@ -28,9 +28,32 @@ def stations(request: HttpRequest) -> QuerySet:
     )
 
 
+def formats(request: HttpRequest) -> QuerySet:
+    """Return the formats the user has permission to view.
+
+    If request is None, return an empty queryset.
+
+    Args:
+        request (HttpRequest): Request object.
+
+    Returns:
+        QuerySet: Stations the user has permission to view
+    """
+    if request is None:
+        return DataImport.objects.none()
+
+    user = request.user
+    return (
+        get_objects_for_user(user, "importing.view_dataimport", klass=DataImport)
+        .values_list("format__name", flat=True)
+        .distinct()
+    )
+
+
 class DataImportFilter(FilterSet):
     station = filters.ModelChoiceFilter(queryset=stations)
+    format = filters.ModelChoiceFilter(queryset=formats)
 
     class Meta:
         model = DataImport
-        fields = ["station", "status"]
+        fields = ["station", "format", "status"]
