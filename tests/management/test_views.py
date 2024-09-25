1c44df287f368416f85068edcd65b56fb7303681
Diego Alonso Alvarez <d.alonso-alvarez@imperial.ac.uk>
2024-09-25 09:16:42 +0100

    :white_check_mark: Add TestCustomDetailView.

--- tests/management/test_views.py
+++ tests/management/test_views.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import PermissionDenied
 from django.test import RequestFactory, TestCase
 from guardian.shortcuts import assign_perm, get_user_model
 
@@ -85,3 +86,82 @@ class TestCustomTableView(TestCase):
         self.assertEqual(view.list_url, "sensor:sensor_list")
         self.assertEqual(view.create_url, "sensor:sensor_create")
         self.assertEqual(view.title, "Sensors")
+
+
+class TestCustomDetailView(TestCase):
+    def setUp(self) -> None:
+        from sensor.models import Sensor, SensorType
+
+        User = get_user_model()
+        self.factory = RequestFactory()
+        self.user = User.objects.create_user(username="testuser", password="12345")
+        self.sensor_type = SensorType.objects.create(name="Temperature")
+        self.sensor = Sensor.objects.create(
+            code="Temperature Sensor",
+            sensor_type=self.sensor_type,
+            model="12345",
+        )
+        assign_perm("view_sensor", self.user, self.sensor)
+        self.sensor_private = Sensor.objects.create(
+            code="Another Sensor",
+            sensor_type=self.sensor_type,
+            model="12345",
+        )
+
+    def test_get_object(self):
+        from management.views import CustomDetailView
+        from sensor.models import Sensor
+
+        request = self.factory.get("/fake-url")
+        request.user = self.user
+        view = CustomDetailView()
+        view.request = request
+        view.kwargs = {"pk": self.sensor.pk}
+        view.model = Sensor
+        obj = view.get_object()
+        self.assertEqual(obj, self.sensor)
+
+    def test_get_object_permission_denied(self):
+        from management.views import CustomDetailView
+        from sensor.models import Sensor
+
+        request = self.factory.get("/fake-url")
+        request.user = self.user
+        view = CustomDetailView()
+        view.request = request
+        view.kwargs = {"pk": self.sensor_private.pk}
+        view.model = Sensor
+        with self.assertRaises(PermissionDenied):
+            view.get_object()
+
+    def test_get_context_data(self):
+        from management.views import CustomDetailView
+        from sensor.models import Sensor
+
+        request = self.factory.get("/fake-url")
+        request.user = self.user
+        view = CustomDetailView()
+        view.request = request
+        view.kwargs = {"pk": self.sensor.pk}
+        view.model = Sensor
+        view.object = view.get_object()
+        context = view.get_context_data()
+        self.assertIn("form", context)
+        self.assertEqual(context["title"], "Sensor")
+        self.assertIsNone(context["delete_url"])
+        self.assertIsNone(context["edit_url"])
+        self.assertIsNone(context["list_url"])
+
+    def test_properties(self):
+        from management.views import CustomDetailView
+        from sensor.models import Sensor
+
+        view = CustomDetailView()
+        view.model = Sensor
+        self.assertEqual(view.app_label, "sensor")
+        self.assertEqual(view.model_name, "sensor")
+        self.assertEqual(view.permission_required, "sensor.view_sensor")
+        self.assertEqual(view.list_url, "sensor:sensor_list")
+        self.assertEqual(view.delete_url, "sensor:sensor_delete")
+        self.assertEqual(view.edit_url, "sensor:sensor_edit")
+        self.assertEqual(view.model_description, "Sensor")
