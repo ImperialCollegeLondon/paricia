79cdab69ad23aa77277bae330085492d94ebf6a4
Diego Alonso Alvarez <d.alonso-alvarez@imperial.ac.uk>
2024-09-27 11:08:58 +0100

    :bug: Fix bug with filters.

--- importing/filters.py
+++ importing/filters.py
@@ -1,58 +1,15 @@
-from django.db.models.query import QuerySet
-from django.http import HttpRequest
 from django_filters import FilterSet, filters
-from guardian.shortcuts import get_objects_for_user
+
+from formatting.models import Format
+from management.filters import FilterVisible
+from station.models import Station
 
 from .models import DataImport
 
 
-def stations(request: HttpRequest) -> QuerySet:
-    """Return the stations the user has permission to view.
-
-    If request is None, return an empty queryset.
-
-    Args:
-        request (HttpRequest): Request object.
-
-    Returns:
-        QuerySet: Stations the user has permission to view
-    """
-    if request is None:
-        return DataImport.objects.none()
-
-    user = request.user
-    return (
-        get_objects_for_user(user, "importing.view_dataimport", klass=DataImport)
-        .values_list("station__station_code", flat=True)
-        .distinct()
-    )
-
-
-def formats(request: HttpRequest) -> QuerySet:
-    """Return the formats the user has permission to view.
-
-    If request is None, return an empty queryset.
-
-    Args:
-        request (HttpRequest): Request object.
-
-    Returns:
-        QuerySet: Stations the user has permission to view
-    """
-    if request is None:
-        return DataImport.objects.none()
-
-    user = request.user
-    return (
-        get_objects_for_user(user, "importing.view_dataimport", klass=DataImport)
-        .values_list("format__name", flat=True)
-        .distinct()
-    )
-
-
 class DataImportFilter(FilterSet):
-    station = filters.ModelChoiceFilter(queryset=stations)
-    format = filters.ModelChoiceFilter(queryset=formats)
+    station = filters.ModelChoiceFilter(queryset=FilterVisible(DataImport, Station))
+    format = filters.ModelChoiceFilter(queryset=FilterVisible(DataImport, Format))
 
     class Meta:
         model = DataImport
