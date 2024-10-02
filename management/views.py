9001bde92dac06f658aaa60eca280f3ac84f373c
Diego Alonso Alvarez <d.alonso-alvarez@imperial.ac.uk>
2024-10-02 10:53:12 +0100

    :bug: Use a normal form with disabled entries in the detail view.

--- management/views.py
+++ management/views.py
@@ -1,9 +1,7 @@
-from contextlib import suppress
-
 from django import forms
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.exceptions import PermissionDenied
-from django.db.models import ForeignKey, Model
+from django.db.models import Model
 from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DetailView
@@ -14,29 +12,6 @@ from guardian.shortcuts import get_objects_for_user
 
 from .forms import CustomUserCreationForm
 from .permissions import get_queryset
-
-
-def model_to_dict(instance: Model) -> dict:
-    """Convert a model instance to a dictionary.
-
-    For ForeignKey fields, the related model instance is converted to a string, so that
-    a meaningful representation is shown in the dictionary instead of the primary key.
-
-    Args:
-        instance (Model): Model instance to convert.
-
-    Returns:
-        dict: Dictionary with the model instance data.
-    """
-    data = {}
-    for field in instance._meta.fields:
-        data[field.name] = field.value_from_object(instance)
-        if isinstance(field, ForeignKey):
-            with suppress(field.related_model.DoesNotExist):
-                data[field.name] = str(
-                    field.related_model.objects.get(pk=data[field.name])
-                )
-    return data
 
 
 class SignUpView(CreateView):
@@ -71,6 +46,7 @@ class CustomDetailView(LoginRequiredMixin, DetailView):
 
     template_name: str = "object_detail.html"
     show_delete_btn: bool = False
+    fields = "__all__"
 
     def get_object(self) -> Model:
         obj = get_object_or_404(self.model, pk=self.kwargs["pk"])
@@ -82,9 +58,15 @@ class CustomDetailView(LoginRequiredMixin, DetailView):
         class DetailForm(forms.ModelForm):
             class Meta:
                 model = self.model
-                fields = "__all__"
+                fields = self.fields
 
-        return DetailForm(data=model_to_dict(self.object))
+            def __init__(self, *args, **kwargs):
+                super().__init__(*args, **kwargs)
+                for field in self.fields.keys():
+                    self.fields[field].widget.attrs["disabled"] = True
+                    self.fields[field].widget.attrs["readonly"] = True
+
+        return DetailForm(instance=self.object)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
