978b7aea523d7ba79db12be6b487a2dc250a2099
Diego Alonso Alvarez <d.alonso-alvarez@imperial.ac.uk>
2024-10-10 06:56:46 +0100

    :sparkles: Add default owner when importing initial data.

--- utilities/load_initial_data.py
+++ utilities/load_initial_data.py
@@ -3,10 +3,24 @@ etc. Currently data is stored in home/data but this will change in the future wh
 home is refactored.
 """
 
+import json
+import shutil
+import tempfile
+
 from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.core.management import execute_from_command_line
 
 from measurement.models import Measurement
+
+# Get the superuser (the first one, if there are more than one)
+User = get_user_model()
+superuser = User.objects.filter(is_superuser=True).first()
+if not superuser:
+    raise ValueError("Superuser not found. Initial data cannot be loaded.")
+
+# Create a temporary directory for the media files updated with the superuser
+temp_dir = tempfile.TemporaryDirectory()
 
 # The order matters so that foreignkey objects exist when an entry is created.
 # The files are named app_model
@@ -33,6 +47,24 @@ data_files = [
     "measurement_airtemperature",
 ]
 
+for file in data_files:
+    # Measurement data is handled separately as it has no owner
+    if "measurement" in file:
+        shutil.copyfile(
+            settings.BASE_DIR + "/utilities/data/" + file + ".json",
+            temp_dir.name + "/" + file + ".json",
+        )
+        continue
+
+    with open(settings.BASE_DIR + "/utilities/data/" + file + ".json") as f:
+        data = json.load(f)
+
+    for entry in data:
+        entry["fields"]["owner"] = superuser.pk
+
+    with open(temp_dir.name + "/" + file + ".json", "w") as f:
+        json.dump(data, f)
+
 # It's initially necessary to delete all measurements to avoid foreign key constraints
 # And because this is "initial data", after all.
 Measurement.objects.all().delete()
@@ -43,7 +75,7 @@ for file in data_files:
         [
             "manage.py",
             "loaddata",
-            settings.BASE_DIR + "/utilities/data/" + file + ".json",
+            temp_dir.name + "/" + file + ".json",
         ]
     )
 
