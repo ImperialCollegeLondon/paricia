cc09b8ed41aa92b8799d7be52327702a08b0e5ea
Diego Alonso Alvarez <d.alonso-alvarez@imperial.ac.uk>
2024-10-16 15:50:32 +0100

    :recycle: Export a CSV a cleaner dataset.

--- measurement/dash_apps/data_report.py
+++ measurement/dash_apps/data_report.py
@@ -166,7 +166,9 @@ def download_csv_report(
                     end_time=end_time,
                     report_type=temporality,
                 )
-                .drop(columns=["station", "variable"])
+                .drop(columns=["station", "variable", "data_import_id"])
+                .dropna(axis=1, how="all")
+                .dropna(axis=0)
                 .to_csv(index=False)
             )
         except Exception as e:
