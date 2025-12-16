bd62623d019e0c5af856e6a0eeafa106c58453f9
sahil raja <sahilraja100@gmail.com>
2025-12-16 14:29:51 +0000

    undo serializer change

--- importing/serializers.py
+++ importing/serializers.py
@@ -1,21 +1,40 @@
 from rest_framework import serializers
 
+from formatting.models import Format
+from station.models import Station
+
 from .models import DataImport
 
 
-class DataImportUploadRequestSerializer(serializers.ModelSerializer):
+class DataImportUploadRequestSerializer(serializers.Serializer):
     """Serializer for data import upload request."""
 
-    class Meta:
-        model = DataImport
-        fields = [
-            "station",
-            "format",
-            "visibility",
-            "reprocess",
-            "observations",
-            "rawfile",
-        ]
+    station = serializers.SlugRelatedField(
+        slug_field="station_code",
+        queryset=Station.objects.all(),
+        help_text="Station code",
+    )
+    format = serializers.PrimaryKeyRelatedField(
+        queryset=Format.objects.all(),
+        help_text="Format ID",
+    )
+    visibility = serializers.ChoiceField(
+        choices=["public", "private"],
+        default="private",
+        help_text="Visibility level",
+    )
+    reprocess = serializers.BooleanField(
+        default=False,
+        help_text="Reprocess data after import",
+    )
+    observations = serializers.CharField(
+        required=False,
+        allow_blank=True,
+        help_text="Additional observations",
+    )
+    rawfile = serializers.FileField(
+        help_text="Data file to upload",
+    )
 
 
 class DataImportUploadResponseSerializer(serializers.ModelSerializer):
