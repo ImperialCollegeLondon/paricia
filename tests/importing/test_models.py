4eff5588f91f835ce0518e80c217ebc206863426
Sahil Raja <sraja@ic.ac.uk>
2026-02-10 17:49:06 +0000

    adding tests

--- tests/importing/test_models.py
+++ tests/importing/test_models.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 
 
@@ -66,3 +67,105 @@ class TestSaveImportModels(TestCase):
         retrieved_dit = DataImport.objects.get_queryset()[0]
         self.assertEqual(retrieved_dit.station, self.station)
         self.assertEqual(retrieved_dit.format, self.file_format)
+
+
+class TestThingsboardImportMap(TestCase):
+    fixtures = [
+        "management_user",
+        "variable_unit",
+        "variable_variable",
+        "sensor_type",
+        "sensor_brand",
+        "station_country",
+        "station_region",
+        "station_ecosystem",
+        "station_institution",
+        "station_type",
+        "station_place",
+        "station_basin",
+        "station_placebasin",
+        "station_station",
+    ]
+
+    def setUp(self):
+        from sensor.models import Sensor, SensorBrand, SensorType
+        from station.models import Station
+        from variable.models import SensorInstallation, Variable
+
+        self.station = Station.objects.get(station_id=8)
+        self.variable = Variable.objects.get(variable_id=1)
+        self.other_variable = Variable.objects.get(variable_id=2)
+
+        sensor_type = SensorType.objects.get(type_id=1)
+        sensor_brand = SensorBrand.objects.get(brand_id=1)
+        self.sensor = Sensor.objects.create(
+            code="TEST_SENSOR_01",
+            sensor_type=sensor_type,
+            sensor_brand=sensor_brand,
+            owner=self.station.owner,
+        )
+
+        self.sensor_installation = SensorInstallation.objects.create(
+            variable=self.variable,
+            station=self.station,
+            sensor=self.sensor,
+            start_date="2020-01-01",
+            owner=self.station.owner,
+        )
+
+    def test_create_thingsboard_import_map(self):
+        from importing.models import ThingsboardImportMap
+
+        mapping = ThingsboardImportMap.objects.create(
+            name_id="Test Mapping",
+            var_id=self.variable,
+            device_id="tb-device-001",
+            station=self.station,
+        )
+
+        retrieved = ThingsboardImportMap.objects.get(pk=mapping.pk)
+        self.assertEqual(retrieved.name_id, "Test Mapping")
+        self.assertEqual(retrieved.var_id, self.variable)
+        self.assertEqual(retrieved.device_id, "tb-device-001")
+        self.assertEqual(retrieved.station, self.station)
+
+    def test_str(self):
+        from importing.models import ThingsboardImportMap
+
+        mapping = ThingsboardImportMap(
+            name_id="Test Mapping",
+            var_id=self.variable,
+            device_id="tb-device-001",
+            station=self.station,
+        )
+
+        self.assertEqual(
+            str(mapping),
+            f"Test Mapping - {self.variable} - tb-device-001",
+        )
+
+    def test_clean_valid_variable(self):
+        from importing.models import ThingsboardImportMap
+
+        mapping = ThingsboardImportMap(
+            name_id="Valid Mapping",
+            var_id=self.variable,
+            device_id="tb-device-002",
+            station=self.station,
+        )
+        # Should not raise any errors
+        mapping.clean()
+
+    def test_clean_invalid_variable(self):
+        from importing.models import ThingsboardImportMap
+
+        mapping = ThingsboardImportMap(
+            name_id="Invalid Mapping",
+            var_id=self.other_variable,
+            device_id="tb-device-003",
+            station=self.station,
+        )
+        with self.assertRaises(ValidationError) as ctx:
+            mapping.clean()
+
+        self.assertIn("variable", ctx.exception.message_dict)
