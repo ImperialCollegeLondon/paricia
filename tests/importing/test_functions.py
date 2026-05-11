8b093f30b1d31a89b8c5c04f7a9c66e97ae23132
Stephanie Wills <stephanie.wills13@imperial.ac.uk>
2026-05-11 11:24:33 +0100

    Add missing tests

--- tests/importing/test_functions.py
+++ tests/importing/test_functions.py
@@ -1,11 +1,13 @@
 from datetime import datetime
 from pathlib import Path
-from unittest.mock import Mock
+from unittest.mock import Mock, patch
 
 import numpy as np
 import pandas as pd
 import pytz
 from django.test import TestCase
+
+from formatting.models import Format
 
 
 class TestMatrixFunctions(TestCase):
@@ -31,7 +33,6 @@ class TestMatrixFunctions(TestCase):
     ]
 
     def setUp(self):
-        from formatting.models import Format
         from importing.models import DataImport
         from station.models import TIMEZONES, Station
 
@@ -54,7 +55,7 @@ class TestMatrixFunctions(TestCase):
         df = read_data_to_import(
             self.data_file, self.file_format, self.station.timezone
         )
-        self.assertEqual(df.shape, (263371, 5))
+        self.assertEqual(df.shape, (263371, 4))
 
     def test_validate_values(self):
         from importing.functions import validate_values
@@ -82,12 +83,12 @@ class TestMatrixFunctions(TestCase):
         )
         expected_columns = [("date", "date"), (1, "value")]
 
-        data, columns = validate_values(mock_classification, matrix)
+        data, columns = validate_values(matrix, mock_classification)
         pd.testing.assert_frame_equal(data, expected_data)
         self.assertEqual(columns, expected_columns)
 
-    def test_clean_dataframe(self):
-        from importing.functions import clean_dataframe
+    def test_remove_nan_rows(self):
+        from importing.functions import remove_nan_rows
 
         data = pd.DataFrame(
             {
@@ -96,7 +97,6 @@ class TestMatrixFunctions(TestCase):
                 "maximum": [15.0, 25.0, 35.0, np.nan],
             }
         )
-        matrix = data.copy()
         columns = [("date", "date"), (1, "value"), (2, "maximum")]
 
         mock_classification = Mock()
@@ -110,7 +110,7 @@ class TestMatrixFunctions(TestCase):
                 "maximum": [15.0, 25.0, 35.0],
             }
         )
-        cleaned_data = clean_dataframe(data, mock_classification, matrix, columns)
+        cleaned_data = remove_nan_rows(data, mock_classification, columns)
         pd.testing.assert_frame_equal(cleaned_data, expected_data)
 
         # Check null data raises error
@@ -123,7 +123,7 @@ class TestMatrixFunctions(TestCase):
         )
 
         with self.assertRaises(ValueError):
-            clean_dataframe(data, mock_classification, matrix, columns)
+            remove_nan_rows(data, mock_classification, columns)
 
     def test_process_incremental_data(self):
         from importing.functions import process_incremental_data
@@ -186,6 +186,88 @@ class TestMatrixFunctions(TestCase):
         ).reset_index(drop=True)
         pd.testing.assert_frame_equal(expected_data, processed_data)
 
+    @patch("importing.functions.validate_values")
+    @patch("importing.functions.standardise_floats")
+    @patch("importing.functions.remove_nan_rows")
+    @patch("importing.functions.process_incremental_data")
+    @patch("importing.functions.process_cumulative_data")
+    def test_get_processed_variable_data(
+        self, cum_mock, incr_mock, nan_mock, floats_mock, validate_mock
+    ):
+        """Test the get_processed_variable_data function."""
+        from importing.functions import get_processed_variable_data
+
+        dates = pd.to_datetime(
+            [
+                datetime(2026, 1, 1, 9, 0, 0),
+                datetime(2026, 1, 1, 9, 5, 30),
+                datetime(2026, 1, 1, 9, 15, 0),
+                datetime(2026, 1, 1, 9, 17, 20),
+                datetime(2026, 1, 1, 9, 22, 47),
+            ]
+        )
+        matrix = pd.DataFrame(
+            {
+                "date": dates,
+                "value": [10.0, 25.0, 37.0, 40.0, 52.0],
+            }
+        )
+        nan_mock.return_value = matrix
+        floats_mock.return_value = matrix
+        columns = [("date", "date"), ("value", "value")]
+        validate_mock.return_value = (matrix, columns)
+        incr_mock.return_value = matrix
+        cum_mock.return_value = matrix
+
+        # With cumulative data
+        classification_mock = Mock()
+        classification_mock.incremental = True
+        classification_mock.accumulate = 3
+        classification_mock.resolution = 2
+        classification_mock.variable.variable_id = 4
+        station_mock = Mock()
+        station_mock.station_id = 1
+        import_mock = Mock()
+        import_mock.data_import_id = 7
+        start_mock, end_mock = Mock(), Mock()
+
+        expected_data = pd.DataFrame(
+            {
+                "date": dates,
+                "value": [10.0, 25.0, 37.0, 40.0, 52.0],
+                "station_id": [1] * 5,
+                "variable_id": [4] * 5,
+                "data_import_id": [7] * 5,
+            }
+        )
+        result = get_processed_variable_data(
+            matrix, classification_mock, station_mock, import_mock, start_mock, end_mock
+        )
+        pd.testing.assert_frame_equal(expected_data, result)
+        nan_mock.assert_called_once_with(matrix, classification_mock, columns)
+        floats_mock.assert_called_once_with(matrix, classification_mock)
+        validate_mock.assert_called_once_with(matrix, classification_mock)
+        incr_mock.assert_called_once_with(matrix)
+        cum_mock.assert_called_once_with(
+            matrix, classification_mock, 3, start_mock, end_mock
+        )
+
+        # Without cumulative data
+        classification_mock.accumulate = False
+        expected_data = pd.DataFrame(
+            {
+                "date": dates,
+                "value": [i * 2 for i in [10.0, 25.0, 37.0, 40.0, 52.0]],
+                "station_id": [1] * 5,
+                "variable_id": [4] * 5,
+                "data_import_id": [7] * 5,
+            }
+        )
+        result = get_processed_variable_data(
+            matrix, classification_mock, station_mock, import_mock, start_mock, end_mock
+        )
+        pd.testing.assert_frame_equal(expected_data, result)
+
     def test_construct_matrix(self):
         from importing.functions import construct_matrix
         from variable.models import Variable
@@ -194,6 +276,8 @@ class TestMatrixFunctions(TestCase):
             self.data_file, self.file_format, self.station, self.data_import
         )
         self.assertEqual(len(variables_data), 2)
+        self.assertEqual(len(variables_data[0]), 263370)
+        self.assertEqual(len(variables_data[1]), 263370)
         vars = list(
             Variable.objects.filter(
                 variable_id__in=[var["variable_id"][0] for var in variables_data]
@@ -208,6 +292,50 @@ class TestMatrixFunctions(TestCase):
 
         self.assertEqual(data_dict["waterlevel"].value.min(), 0.0)
         self.assertEqual(data_dict["waterlevel"].value.max(), 96.54)
+
+    def test_construct_matrix_no_classifications(self):
+        """Test construct_matrix raises an error with no classifications."""
+        from importing.functions import construct_matrix
+
+        format = self.file_format
+        format.format_id = 1000
+        with self.assertRaises(ValueError) as msg:
+            construct_matrix(self.data_file, format, self.station, self.data_import)
+
+        self.assertEqual(
+            str(msg.exception),
+            "No classifications found for this format. Please add some.",
+        )
+
+    @patch("importing.functions.read_data_to_import")
+    def test_construct_matrix_max_cols(self, read_data_mock):
+        """Test construct_matrix raises an error when there are not enough columns."""
+        from importing.functions import construct_matrix
+
+        read_data_mock.return_value = pd.DataFrame(
+            {
+                "date": ["2022-01-01 10:00:00", "2022-01-02 12:00:00"],
+                "value": [1, 2],
+            }
+        )
+        with self.assertRaises(ValueError) as msg:
+            construct_matrix(
+                self.data_file, self.file_format, self.station, self.data_import
+            )
+        self.assertStartsWith(str(msg.exception), "The number of columns in the file")
+
+    @patch("importing.functions.construct_matrix")
+    def test_save_temp_data_to_permanent_no_data(self, matrix_mock):
+        """Test save_temp_data_to_permanent raises an error when no data to import."""
+        from importing.functions import save_temp_data_to_permanent
+
+        matrix_mock.return_value = []
+        with self.assertRaises(ValueError) as msg:
+            save_temp_data_to_permanent(self.data_import)
+
+        self.assertEqual(
+            str(msg.exception), "No data to import. Is the chosen format correct?"
+        )
 
 
 class TestDateFunctions(TestCase):
@@ -235,7 +363,6 @@ class TestDateFunctions(TestCase):
     def setUp(self):
         from django.core.files.uploadedfile import SimpleUploadedFile
 
-        from formatting.models import Format
         from importing.functions import read_data_to_import
         from importing.models import DataImport
         from measurement.models import Measurement
@@ -285,55 +412,77 @@ class TestDateFunctions(TestCase):
             value=5.7,
         )
 
-    def test_get_last_uploaded_date(self):
-        from importing.functions import get_last_uploaded_date
 
-        self.assertEqual(get_last_uploaded_date(8, "flow").year, 2016)
+class TestReadFile(TestCase):
+    def test_read_file_excel(self):
+        import io
 
-    def test_validate_dates(self):
-        from importing.functions import validate_dates
+        from formatting.models import Delimiter
+        from importing.functions import read_file_excel
 
-        validation = validate_dates(self.data_import)
-        self.assertEqual(validation[0][0]["variable_code"], "flow")
-        self.assertTrue(validation[0][0]["exists"])
-        self.assertEqual(validation[0][1]["variable_code"], "waterlevel")
-        self.assertFalse(validation[0][1]["exists"])
-        self.assertEqual(validation[1], True)
+        # Create a sample excel file
+        data = pd.DataFrame([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+        excel_file = io.BytesIO()
+        with pd.ExcelWriter(excel_file, engine="openpyxl") as writer:
+            data.to_excel(writer, index=False, header=False)
 
+        # File format skipping one row and using a comma as delimiter
+        file_format = Format(
+            first_row=1, delimiter=Delimiter(name="comma", character=",")
+        )
+        result = read_file_excel(excel_file, file_format)
+        expected_result = [[4, 5, 6], [7, 8, 9]]
+        self.assertEqual(result.values.tolist(), expected_result)
 
-class TestReadFileCSV(TestCase):
+        # File format using the first row and using a comma as delimiter
+        excel_file.seek(0)
+        file_format = Format(
+            first_row=0, delimiter=Delimiter(name="comma", character=",")
+        )
+        result = read_file_excel(excel_file, file_format)
+        expected_result = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+        self.assertEqual(result.values.tolist(), expected_result)
+
+        # File format skipping the last row and using a comma as delimiter
+        excel_file.seek(0)
+        file_format = Format(
+            first_row=0, footer_rows=1, delimiter=Delimiter(name="comma", character=",")
+        )
+        result = read_file_excel(excel_file, file_format)
+        expected_result = [[1, 2, 3], [4, 5, 6]]
+        self.assertEqual(result.values.tolist(), expected_result)
+
     def test_read_file_csv(self):
         import io
 
-        from formatting.models import Delimiter, Format
+        from formatting.models import Delimiter
         from importing.functions import read_file_csv
 
         # Create a sample CSV file
         csv_data = "1,2,3\n4,5,6\n7,8,9\n"
         csv_file = io.StringIO(csv_data)
 
-        # File format skipping one row
-        # and using a coma as delimiter
+        # File format skipping one row and using a comma as delimiter
         file_format = Format(
-            first_row=1, delimiter=Delimiter(name="coma", character=",")
+            first_row=1, delimiter=Delimiter(name="comma", character=",")
         )
         result = read_file_csv(csv_file, file_format)
         expected_result = [[4, 5, 6], [7, 8, 9]]
         self.assertEqual(result.values.tolist(), expected_result)
 
-        # File format using the first row and a coma as delimiter
+        # File format using the first row and using a comma as delimiter
         csv_file.seek(0)
         file_format = Format(
-            first_row=0, delimiter=Delimiter(name="coma", character=",")
+            first_row=0, delimiter=Delimiter(name="comma", character=",")
         )
         result = read_file_csv(csv_file, file_format)
         expected_result = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
         self.assertEqual(result.values.tolist(), expected_result)
 
-        # File format skipping the last row and using a coma as delimiter
+        # File format skipping the last row and using a comma as delimiter
         csv_file.seek(0)
         file_format = Format(
-            first_row=0, footer_rows=1, delimiter=Delimiter(name="coma", character=",")
+            first_row=0, footer_rows=1, delimiter=Delimiter(name="comma", character=",")
         )
         result = read_file_csv(csv_file, file_format)
         expected_result = [[1, 2, 3], [4, 5, 6]]
@@ -344,13 +493,13 @@ class TestProcessDatetimeColumns(TestCase):
     def test_process_datetime_columns_same_column(self):
         import pandas as pd
 
-        from formatting.models import Date, Delimiter, Format, Time
+        from formatting.models import Date, Delimiter, Time
         from importing.functions import process_datetime_columns
 
         # Prepare test data
         data = pd.DataFrame(
             {
-                "datetime": ["2022-01-01 10:00:00", "2022-01-02 12:00:00"],
+                "date": ["2022-01-01 10:00:00", "2022-01-02 12:00:00"],
                 "value": [1, 2],
             }
         )
@@ -360,7 +509,7 @@ class TestProcessDatetimeColumns(TestCase):
             time_column=0,
             date=Date(date_format="", code="%Y-%m-%d"),
             time=Time(time_format="", code="%H:%M:%S"),
-            delimiter=Delimiter(name="coma", character=","),
+            delimiter=Delimiter(name="comma", character=","),
         )
         timezone = "America/New_York"
 
@@ -384,7 +533,7 @@ class TestProcessDatetimeColumns(TestCase):
     def test_process_datetime_columns_different_columns(self):
         import pandas as pd
 
-        from formatting.models import Date, Delimiter, Format, Time
+        from formatting.models import Date, Delimiter, Time
         from importing.functions import process_datetime_columns
 
         # Prepare test data
@@ -421,3 +570,113 @@ class TestProcessDatetimeColumns(TestCase):
         pd.testing.assert_frame_equal(
             processed_data[["date", "value"]], expected_data, check_dtype=False
         )
+
+    def test_process_datetime_different_formats(self):
+        import pandas as pd
+
+        from formatting.models import Date, Delimiter, Time
+        from importing.functions import process_datetime_columns
+
+        # Prepare test data
+        data = pd.DataFrame(
+            {
+                "date": [
+                    datetime(2022, 1, 1, 10, 0, 0),
+                    "2022-01-01 09:00:00",
+                    "not a date",
+                    np.datetime64("2022-01-01T11:00:00"),
+                ],
+                "value": [1, 2, 3, 4],
+            }
+        )
+        file_format = Format(
+            first_row=0,
+            date_column=0,
+            time_column=0,
+            date=Date(date_format="", code="%Y-%m-%d"),
+            time=Time(time_format="", code="%H:%M:%S"),
+            delimiter=Delimiter(name="comma", character=","),
+        )
+        timezone = "America/New_York"
+
+        # Call the function
+        processed_data = process_datetime_columns(data, file_format, timezone)
+
+        # Assert the result
+        expected_data = pd.DataFrame(
+            {
+                "date": [
+                    pd.Timestamp("2022-01-01 09:00:00", tz="America/New_York"),
+                    pd.Timestamp("2022-01-01 10:00:00", tz="America/New_York"),
+                    pd.Timestamp("2022-01-01 11:00:00", tz="America/New_York"),
+                    pd.NaT,
+                ],
+                "value": [2, 1, 4, 3],
+            }
+        )
+        pd.testing.assert_frame_equal(processed_data, expected_data, check_dtype=False)
+
+
+class TestStandardiseFloats(TestCase):
+    def test_standardise_floats_remove_comma(self):
+        from importing.functions import standardise_floats
+
+        # Commas removed entirely
+        df = pd.DataFrame(
+            {
+                "date": pd.date_range("2026-01-01", periods=4),
+                "values": ["10.4,", "10.5,", "10.6", "10:7"],
+            }
+        )
+        mock_classification = Mock()
+        mock_classification.decimal_comma = False
+        expected_df = pd.DataFrame(
+            {
+                "date": pd.date_range("2026-01-01", periods=4),
+                "values": [10.4, 10.5, 10.6, np.nan],
+            }
+        )
+        result = standardise_floats(df, mock_classification)
+        pd.testing.assert_frame_equal(result, expected_df)
+
+    def test_standardise_floats_numeric(self):
+        from importing.functions import standardise_floats
+
+        # Values provided area already floats
+        df = pd.DataFrame(
+            {
+                "date": pd.date_range("2026-01-01", periods=4),
+                "values": [10.4, 10.5, 10.6, np.nan],
+            }
+        )
+        mock_classification = Mock()
+        mock_classification.decimal_comma = False
+        expected_df = pd.DataFrame(
+            {
+                "date": pd.date_range("2026-01-01", periods=4),
+                "values": [10.4, 10.5, 10.6, np.nan],
+            }
+        )
+        result = standardise_floats(df, mock_classification)
+        pd.testing.assert_frame_equal(result, expected_df)
+
+    def test_standardise_floats_replace_commas(self):
+        from importing.functions import standardise_floats
+
+        # Periods removed and commas replaced by periods
+        df = pd.DataFrame(
+            {
+                "date": pd.date_range("2026-01-01", periods=4),
+                "values": ["10,4.", "10,5.", "10,6", "10:7"],
+            }
+        )
+        mock_classification = Mock()
+        mock_classification.decimal_comma = True
+        expected_df = pd.DataFrame(
+            {
+                "date": pd.date_range("2026-01-01", periods=4),
+                "values": [10.4, 10.5, 10.6, np.nan],
+            }
+        )
+        result = standardise_floats(df, mock_classification)
+        pd.testing.assert_frame_equal(result, expected_df)
